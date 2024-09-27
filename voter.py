0361b9f1e90d72c15624d44d44df39b9c72b911e
Will Bowers <willb0102@gmail.com>
2024-09-26 19:52:21 -0600

    Split voter out to its own class

---
+++ voter.py
@@ -0,0 +1,16 @@
+from math import dist
+from numpy import nanargmin, signedinteger, nan
+from typing import Self, Any
+
+
+class Voter(object):
+    _p = [0, 0] # Preference space
+
+    def __init__(self, p):
+        self._p = p
+
+    def dist_to(self, other : Self) -> float:
+        return dist(self._p, other._p) if other is not None else nan
+
+    def pick_nearest(self, candidates : list[Self]) -> signedinteger[Any]:
+        return nanargmin([self.dist_to(c) for c in candidates]) # For now, candidates are Voter as well
